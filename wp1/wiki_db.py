eb27bc01988ee43a79d4b9530bc4c7ece1c0f7b8
Restyled.io <commits@restyled.io>
2019-10-13 23:51:34 +0000

    Restyled by yapf

--- wp1/wiki_db.py
+++ wp1/wiki_db.py
@@ -10,34 +10,33 @@ logger = logging.getLogger(__name__)
 RETRY_TIME_SECONDS = 5
 
 try:
-    from wp1.credentials import WIKI_CREDS
+  from wp1.credentials import WIKI_CREDS
 
-    def connect():
-        kwargs = {
-            "charset": None,
-            "use_unicode": False,
-            "cursorclass": pymysql.cursors.SSDictCursor,
-            **WIKI_CREDS,
-        }
+  def connect():
+    kwargs = {
+        "charset": None,
+        "use_unicode": False,
+        "cursorclass": pymysql.cursors.SSDictCursor,
+        **WIKI_CREDS,
+    }
 
-        tries = 4
-        while True:
-            try:
-                return pymysql.connect(**kwargs)
-            except pymysql.err.InternalError:
-                if tries > 0:
-                    logging.warning(
-                        "Could not connect to database, retrying in %s seconds",
-                        RETRY_TIME_SECONDS,
-                    )
-                    time.sleep(RETRY_TIME_SECONDS)
-                    tries -= 1
-                else:
-                    raise
-
+    tries = 4
+    while True:
+      try:
+        return pymysql.connect(**kwargs)
+      except pymysql.err.InternalError:
+        if tries > 0:
+          logging.warning(
+              "Could not connect to database, retrying in %s seconds",
+              RETRY_TIME_SECONDS,
+          )
+          time.sleep(RETRY_TIME_SECONDS)
+          tries -= 1
+        else:
+          raise
 
 except ImportError:
-    # No creds, so return an empty connect method that will blow up. This is only
-    # to satisfy imports.
-    def connect():
-        pass
+  # No creds, so return an empty connect method that will blow up. This is only
+  # to satisfy imports.
+  def connect():
+    pass
--- wp1/wp10_db.py
+++ wp1/wp10_db.py
@@ -10,36 +10,35 @@ logger = logging.getLogger(__name__)
 RETRY_TIME_SECONDS = 5
 
 try:
-    from wp1.credentials import WP10_CREDS
+  from wp1.credentials import WP10_CREDS
 
-    def connect():
-        kwargs = {
-            "charset": None,
-            "use_unicode": False,
-            "cursorclass": pymysql.cursors.DictCursor,
-            **WP10_CREDS,
-        }
+  def connect():
+    kwargs = {
+        "charset": None,
+        "use_unicode": False,
+        "cursorclass": pymysql.cursors.DictCursor,
+        **WP10_CREDS,
+    }
 
-        tries = 4
-        while True:
-            try:
-                return pymysql.connect(**kwargs)
-            except pymysql.err.InternalError:
-                if tries > 0:
-                    logging.warning(
-                        "Could not connect to database, retrying in %s seconds",
-                        RETRY_TIME_SECONDS,
-                    )
-                    time.sleep(RETRY_TIME_SECONDS)
-                    tries -= 1
-                else:
-                    raise
-
+    tries = 4
+    while True:
+      try:
+        return pymysql.connect(**kwargs)
+      except pymysql.err.InternalError:
+        if tries > 0:
+          logging.warning(
+              "Could not connect to database, retrying in %s seconds",
+              RETRY_TIME_SECONDS,
+          )
+          time.sleep(RETRY_TIME_SECONDS)
+          tries -= 1
+        else:
+          raise
 
 except ImportError:
-    print("import error")
+  print("import error")
 
-    # No creds, so return an empty connect method that will blow up. This is only
-    # to satisfy imports.
-    def connect():
-        return None
+  # No creds, so return an empty connect method that will blow up. This is only
+  # to satisfy imports.
+  def connect():
+    return None
