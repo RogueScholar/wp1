5c90726bc896d7b072601f2caab25dfc598b07ca
Restyled.io <commits@restyled.io>
2019-11-25 14:23:34 +0000

    Restyled by black

--- wp1/web/app.py
+++ wp1/web/app.py
@@ -12,9 +12,10 @@ from wp1.web.db import get_db, has_db
 def get_redis_creds():
     try:
         from wp1.credentials import REDIS_CREDS
+
         return REDIS_CREDS
     except ImportError:
-        print('No REDIS_CREDS found, using defaults.')
+        print("No REDIS_CREDS found, using defaults.")
         return None
 
 
@@ -22,39 +23,39 @@ def create_app():
     app = flask.Flask(__name__)
     cors = flask_cors.CORS(app)
 
-    rq_user = os.environ.get('RQ_USER')
-    rq_pass = os.environ.get('RQ_PASS')
+    rq_user = os.environ.get("RQ_USER")
+    rq_pass = os.environ.get("RQ_PASS")
     redis_creds = get_redis_creds()
 
     if rq_user is not None and rq_pass is not None:
         app.config.from_object(rq_dashboard.default_settings)
         if redis_creds is not None:
-            app.config.update({'RQ_DASHBOARD_REDIS_HOST': redis_creds['host']})
-            app.config.update({'RQ_DASHBOARD_REDIS_PORT': redis_creds['port']})
+            app.config.update({"RQ_DASHBOARD_REDIS_HOST": redis_creds["host"]})
+            app.config.update({"RQ_DASHBOARD_REDIS_PORT": redis_creds["port"]})
         add_basic_auth(rq_dashboard.blueprint, rq_user, rq_pass)
         app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
     else:
-        print('No RQ_USER/RQ_PASS found in env. RQ dashboard not created.')
+        print("No RQ_USER/RQ_PASS found in env. RQ dashboard not created.")
 
     @app.teardown_request
     def close_dbs(ex):
-        if has_db('wikidb'):
-            conn = get_db('wikidb')
+        if has_db("wikidb"):
+            conn = get_db("wikidb")
             conn.close()
-        if has_db('wp10db'):
-            conn = get_db('wp10db')
+        if has_db("wp10db"):
+            conn = get_db("wp10db")
             conn.close()
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        wp10db = get_db('wp10db')
+        wp10db = get_db("wp10db")
         count = logic_project.count_projects(wp10db)
-        return flask.render_template('index.html.jinja2', count=count)
+        return flask.render_template("index.html.jinja2", count=count)
 
-    @app.route('/projects/count')
+    @app.route("/projects/count")
     def count_projects():
-        wp10db = get_db('wp10db')
+        wp10db = get_db("wp10db")
         count = logic_project.count_projects(wp10db)
-        return flask.jsonify({'count': count})
+        return flask.jsonify({"count": count})
 
     return app
--- wp1/web/app_test.py
+++ wp1/web/app_test.py
@@ -9,82 +9,85 @@ from wp1.web.base_web_testcase import BaseWebTestcase
 
 
 class AppTest(BaseWebTestcase):
-
     def test_index(self):
         with self.override_db(self.app), self.app.test_client() as client:
-            rv = client.get('/')
-            self.assertTrue(b'Wikipedia 1.0 Server' in rv.data)
+            rv = client.get("/")
+            self.assertTrue(b"Wikipedia 1.0 Server" in rv.data)
 
     def test_index_project_count(self):
         projects = []
         for i in range(101):
-            projects.append({
-                'p_project': b'Project %s' % str(i).encode('utf-8'),
-                'p_timestamp': b'20181225'
-            })
+            projects.append(
+                {
+                    "p_project": b"Project %s" % str(i).encode("utf-8"),
+                    "p_timestamp": b"20181225",
+                }
+            )
 
         with self.wp10db.cursor() as cursor:
             cursor.executemany(
-                'INSERT INTO projects (p_project, p_timestamp) '
-                'VALUES (%(p_project)s, %(p_timestamp)s)', projects)
+                "INSERT INTO projects (p_project, p_timestamp) "
+                "VALUES (%(p_project)s, %(p_timestamp)s)",
+                projects,
+            )
         self.wp10db.commit()
 
         with self.override_db(self.app), self.app.test_client() as client:
-            rv = client.get('/')
-            self.assertTrue(b'There are currently 101 projects being tracked '
-                            b'and updated each day.' in rv.data)
+            rv = client.get("/")
+            self.assertTrue(
+                b"There are currently 101 projects being tracked "
+                b"and updated each day." in rv.data
+            )
 
     def test_project_count(self):
         projects = []
         for i in range(101):
-            projects.append({
-                'p_project': b'Project %s' % str(i).encode('utf-8'),
-                'p_timestamp': b'20181225'
-            })
+            projects.append(
+                {
+                    "p_project": b"Project %s" % str(i).encode("utf-8"),
+                    "p_timestamp": b"20181225",
+                }
+            )
 
         with self.wp10db.cursor() as cursor:
             cursor.executemany(
-                'INSERT INTO projects (p_project, p_timestamp) '
-                'VALUES (%(p_project)s, %(p_timestamp)s)', projects)
+                "INSERT INTO projects (p_project, p_timestamp) "
+                "VALUES (%(p_project)s, %(p_timestamp)s)",
+                projects,
+            )
         self.wp10db.commit()
 
         with self.override_db(self.app), self.app.test_client() as client:
-            rv = client.get('/projects/count')
+            rv = client.get("/projects/count")
             data = json.loads(rv.data)
-            self.assertEquals(101, data['count'])
+            self.assertEquals(101, data["count"])
 
 
 class RqTest(unittest.TestCase):
-
-    @patch('wp1.web.app.get_redis_creds')
+    @patch("wp1.web.app.get_redis_creds")
     def test_rq_no_login(self, patched_get_creds):
-        patched_get_creds.return_value = {
-            'host': 'localhost',
-            'port': 6379,
-        }
-        os.environ['RQ_USER'] = 'testuser'
-        os.environ['RQ_PASS'] = 'testpass'  # nosec
+        patched_get_creds.return_value = {"host": "localhost", "port": 6379}
+        os.environ["RQ_USER"] = "testuser"
+        os.environ["RQ_PASS"] = "testpass"  # nosec
         self.app = create_app()
         with self.app.test_client() as client:
-            rv = client.get('/rq', follow_redirects=True)
-            self.assertTrue(b'Please login' in rv.data)
+            rv = client.get("/rq", follow_redirects=True)
+            self.assertTrue(b"Please login" in rv.data)
 
-    @patch('wp1.web.app.get_redis_creds')
+    @patch("wp1.web.app.get_redis_creds")
     def test_rq_login(self, patched_get_creds):
-        patched_get_creds.return_value = {
-            'host': 'localhost',
-            'port': 6379,
-        }
-        os.environ['RQ_USER'] = 'testuser'
-        os.environ['RQ_PASS'] = 'testpass'  # nosec
+        patched_get_creds.return_value = {"host": "localhost", "port": 6379}
+        os.environ["RQ_USER"] = "testuser"
+        os.environ["RQ_PASS"] = "testpass"  # nosec
         self.app = create_app()
         with self.app.test_client() as client:
             rv = client.get(
-                '/rq',
+                "/rq",
                 headers={
-                    'Authorization':
-                        'Basic {}'.format(
-                            b64encode(b'testuser:testpass').decode('utf-8'))
+                    "Authorization": "Basic {}".format(
+                        b64encode(b"testuser:testpass").decode("utf-8")
+                    )
                 },
-                follow_redirects=True)
-            self.assertFalse(b'Please login' in rv.data)
+                follow_redirects=True,
+            )
+            self.assertFalse(b"Please login" in rv.data)
