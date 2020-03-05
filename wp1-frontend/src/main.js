1fb548bb2f2e9bb2649530f31e43c90e921643b9
Restyled.io <commits@restyled.io>
2020-03-05 07:18:29 +0000

    Restyled by prettier

--- wp1-frontend/src/main.js
+++ wp1-frontend/src/main.js
@@ -1,9 +1,9 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-new Vue({render : h => h(App)}).$mount('#app');
+new Vue({ render: h => h(App) }).$mount("#app");
