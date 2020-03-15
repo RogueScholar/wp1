d4383b85be1fd468ebe905ae98e8565c51fec513
Restyled.io <commits@restyled.io>
2020-03-15 21:17:06 +0000

    Restyled by prettier

--- wp1-frontend/src/main.js
+++ wp1-frontend/src/main.js
@@ -1,28 +1,28 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import App from './App.vue';
-import IndexPage from './components/IndexPage.vue';
-import ProjectPage from './components/ProjectPage.vue';
+import App from "./App.vue";
+import IndexPage from "./components/IndexPage.vue";
+import ProjectPage from "./components/ProjectPage.vue";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
 const routes = [
-  {path : '/', component : IndexPage},
-  {path : '/project/:projectName', component : ProjectPage}
+  { path: "/", component: IndexPage },
+  { path: "/project/:projectName", component: ProjectPage }
 ];
 
-const router = new VueRouter({routes});
+const router = new VueRouter({ routes });
 
 new Vue({
-  el : '#app',
-  render : h => h(App),
+  el: "#app",
+  render: h => h(App),
   router,
-  template : '<App/>',
-  components : {App}
+  template: "<App/>",
+  components: { App }
 });
